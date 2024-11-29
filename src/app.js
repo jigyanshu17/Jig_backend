16ba255c69ac7f0399a17bc0048bc7aa6fbe40a1
Jigyanshu Don <jigyanshukumawat@gmail.com>
2024-11-29 13:15:26 +0530

    implemented express methods

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 import express from 'express'; // Importing express
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
+
 export {app} // Exporting the app object
