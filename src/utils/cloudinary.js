5e0d51d2483ec024801cea9002ba42a45f84a6be
Jigyanshu Don <jigyanshukumawat@gmail.com>
2024-12-04 15:21:52 +0530

    final update for registration(postman tested)

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@ import fs from "fs";
  cloudinary.config({
    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
    api_key: process.env.CLOUDINARY_API_KEY,
-   api_secret: process.env.cloudinary_API_SECRET, // Click 'View API Keys' above to copy your API secret
+   api_secret: process.env.CLOUDINARY_API_SECRET, 
  });
 
  const uploadToCloudinary = async (localFilePath) => {
@@ -15,11 +15,12 @@ import fs from "fs";
      const result = await cloudinary.uploader.upload(localFilePath, {
     //    folder: options.folder || "uploads", // Optional: specify a folder
     //    public_id: options.publicId, // Optional: custom public ID
-       resource_type: options.resourceType || "auto", // auto-detect resource type
+       resource_type: "auto", // auto-detect resource type
        
      });
 
-     console.log("Upload successful:", result.url);
+       //  console.log("Upload successful:", result.url);
+       fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got successful  
      return result;
    } catch (error) {
      fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
